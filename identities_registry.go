330560eb09d49b21c89b813786f60f800953b746
Antoine Mercadal <antoine.mercadal@gmail.com>
2019-06-18 18:24:03 -0700

    new: added search api

--- identities_registry.go
+++ identities_registry.go
@@ -95,6 +95,7 @@ var (
 		"revocation":             RevocationIdentity,
 		"role":                   RoleIdentity,
 		"root":                   RootIdentity,
+		"search":                 SearchIdentity,
 		"service":                ServiceIdentity,
 		"servicedependency":      ServiceDependencyIdentity,
 		"squalltag":              SquallTagIdentity,
@@ -214,6 +215,7 @@ var (
 		"revocations":              RevocationIdentity,
 		"roles":                    RoleIdentity,
 		"root":                     RootIdentity,
+		"search":                   SearchIdentity,
 		"services":                 ServiceIdentity,
 		"servicedependencies":      ServiceDependencyIdentity,
 		"squalltags":               SquallTagIdentity,
@@ -659,8 +661,9 @@ var (
 		"revocation": [][]string{
 			[]string{":shard", ":unique", "zone", "zHash"},
 		},
-		"role": nil,
-		"root": nil,
+		"role":   nil,
+		"root":   nil,
+		"search": nil,
 		"service": [][]string{
 			[]string{":shard", ":unique", "zone", "zHash"},
 			[]string{"updateIdempotencyKey"},
@@ -926,6 +929,8 @@ func (f modelManager) Identifiable(identity elemental.Identity) elemental.Identi
 		return NewRole()
 	case RootIdentity:
 		return NewRoot()
+	case SearchIdentity:
+		return NewSearch()
 	case ServiceIdentity:
 		return NewService()
 	case ServiceDependencyIdentity:
@@ -1145,6 +1150,8 @@ func (f modelManager) SparseIdentifiable(identity elemental.Identity) elemental.
 		return NewSparseRevocation()
 	case RoleIdentity:
 		return NewSparseRole()
+	case SearchIdentity:
+		return NewSparseSearch()
 	case ServiceIdentity:
 		return NewSparseService()
 	case ServiceDependencyIdentity:
@@ -1374,6 +1381,8 @@ func (f modelManager) Identifiables(identity elemental.Identity) elemental.Ident
 		return &RevocationsList{}
 	case RoleIdentity:
 		return &RolesList{}
+	case SearchIdentity:
+		return &SearchsList{}
 	case ServiceIdentity:
 		return &ServicesList{}
 	case ServiceDependencyIdentity:
@@ -1593,6 +1602,8 @@ func (f modelManager) SparseIdentifiables(identity elemental.Identity) elemental
 		return &SparseRevocationsList{}
 	case RoleIdentity:
 		return &SparseRolesList{}
+	case SearchIdentity:
+		return &SparseSearchsList{}
 	case ServiceIdentity:
 		return &SparseServicesList{}
 	case ServiceDependencyIdentity:
@@ -1745,6 +1756,7 @@ func AllIdentities() []elemental.Identity {
 		RevocationIdentity,
 		RoleIdentity,
 		RootIdentity,
+		SearchIdentity,
 		ServiceIdentity,
 		ServiceDependencyIdentity,
 		SquallTagIdentity,
@@ -2047,6 +2059,8 @@ func AliasesForIdentity(identity elemental.Identity) []string {
 	case RoleIdentity:
 		return []string{}
 	case RootIdentity:
+		return []string{}
+	case SearchIdentity:
 		return []string{}
 	case ServiceIdentity:
 		return []string{
